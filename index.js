9ef5f255f3eef2a1f62f4dd37c1fa791708c30f0
venki200221 <venki21122@gmail.com>
2023-03-05 01:15:03 +0530

    video

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const openai=require("openai");
 const bodyParser=require('body-parser');
 const axios=require("axios");
 
+// find all the adjectives from an array of words
 
 app.use(bodyParser.json());
 
@@ -18,8 +19,37 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
-var words=[];
+// let adjectives = [];
 
+// app.post("/",async(req,res)=>{
+//    try{
+//     const url=req.body.url;
+//     const deepgram = new Deepgram("ecd31834c4e1c18f545ac8a4d1b06d8151197352");
+//     const fileSource = { url: url };
+//     const response = await deepgram.transcription.preRecorded(fileSource, {
+//         punctuate: true,
+//         detect_topics:true,
+//       });
+//       let getwords=response.results.channels[0].alternatives[0].words;
+//         getwords.forEach(word=>{
+//             words.push(word.word);
+//         })
+//       let trans=response.results.channels[0].alternatives[0].topics;
+
+// for (var i = 0; i < words.length; i++) {
+//   if (words[i].endsWith("y") || words[i].endsWith("ed")) {
+//     adjectives.push(words[i]);
+//   }
+// }
+//       res.send(adjectives);
+//     }
+//     catch(err){
+//         res.send(err);
+// }
+// });
+
+let adjectives = [];
+let words = [];
 app.post("/",async(req,res)=>{
    try{
     const url=req.body.url;
@@ -31,20 +61,30 @@ app.post("/",async(req,res)=>{
       });
       let getwords=response.results.channels[0].alternatives[0].words;
         getwords.forEach(word=>{
-            words.push(word.word);
+            words.push(word.word); // push word to words array instead of adjectives array 
         })
-      let trans=response.results.channels[0].alternatives[0].topics;
-      res.send(words);
-    }
-    catch(err){
-        res.send(err);
-}
-});
 
+      let trans=response.results.channels[0].alternatives[0].topics;
+
+      for (var i = 0; i < words.length; i++) { // use .endsWith() method on elements of words array 
+        if (words[i].endsWith("y") || words[i].endsWith("ed")) {  // push to adjectives array instead of words array 
+          adjectives.push(words[i]);   //  push to adjectives array instead of words array 
+
+        }
+
+      }
+
+      res.send(adjectives);
+
+    } catch(err){
+
+        res.send(err);
+
+    }});
 
 app.get("/image",(req,res)=>{
 
-  const url = 'https://api.envato.com/v1/discovery/search/search/item?term=aws,type=stock-video';
+  const url = `https://api.envato.com/v1/discovery/search/search/item?term=${adjectives},type=stock-video`;
   const headers = {
     'Authorization':'Bearer TBR7vSbLt5ABo9Y5NFbxFYMnAvCYF887'
   };
--- txt.py
+++ txt.py
@@ -1,12 +1,19 @@
-# write an api merge the videos which are provided as the array of  links  to form a single video
-
-
+# merge the videos which are provided as the array of  links  to form a single video
 import moviepy.editor as mp
+import requests 
 
-video_links = ["https://previews.customer.envatousercontent.com/28ed47f2-e455-4fd4-a9f2-fb26916a6f83/watermarked_preview/watermarked_preview.mp4",
- "https://previews.customer.envatousercontent.com/dae70b46-7459-4949-bc25-c653cf52e635/watermarked_preview/watermarked_preview.mp4"]
+# fetch data from api
+url = 'https://video-editor-gold.vercel.app/image' 
+response = requests.get(url) 
+if response.status_code == 200:     
+    video_links = response.json()
+    clips=[]
+    for video in video_links:
+        clips.append(mp.VideoFileClip(video))
+    
+    final_clip = mp.concatenate_videoclips(clips) 
+    final_clip.write_videofile("merged_video.mp4")
+else: 
+    print('An error has occurred.')
 
-video_clips = [mp.VideoFileClip(link) for link in video_links]
-final_clip = mp.concatenate_videoclips(video_clips) 
-final_clip.write_videofile("merged_videos.mp4")
-
+#find all the adjectives from an array of words
